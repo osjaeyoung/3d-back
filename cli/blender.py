11b5706f12e5cdbe7de20f4eb36202dcf2f3bd09
kiminseob <kis6473@naver.com>
2024-10-17 05:48:02 +0000

    feat: blender ci

---
+++ cli/blender.py
@@ -0,0 +1,80 @@
+import bpy
+import argparse
+
+'''
+ci:
+  blender -b -P blender.py -- --ratio 0.5 --inm 'mesh.obj' --outm 'output_mesh.obj'
+'''
+ 
+def get_args():
+  parser = argparse.ArgumentParser()
+ 
+  # get all script args
+  _, all_arguments = parser.parse_known_args()
+  double_dash_index = all_arguments.index('--')
+  script_args = all_arguments[double_dash_index + 1: ]
+ 
+  # add parser rules
+  parser.add_argument('-r', '--ratio', help="Ratio of reduction, Example: 0.5 mean half number of faces ")
+  parser.add_argument('-in', '--inm', help="Original Model")
+  parser.add_argument('-out', '--outm', help="Decimated output file")
+  parsed_script_args, _ = parser.parse_known_args(script_args)
+  return parsed_script_args
+ 
+args = get_args()
+decimateRatio = float(args.ratio)
+print(decimateRatio)
+
+input_model = str(args.inm)
+print(input_model)
+
+output_model = str(args.outm)
+print(output_model)
+
+print('\n Clearing blender scene (default garbage...)')
+# deselect all
+bpy.ops.object.select_all(action='DESELECT')
+
+# selection
+bpy.data.objects['Camera'].select_set(True)
+
+# remove it
+bpy.ops.object.delete() 
+
+# Clear Blender scene
+# select objects by type
+for o in bpy.data.objects:
+    if o.type == 'MESH':
+        o.select_set(True)
+    else:
+        o.select_set(False)
+
+# call the operator once
+bpy.ops.object.delete()
+
+print('\n Beginning the process of Decimation using Blender Python API ...')
+bpy.ops.wm.obj_import(filepath=input_model)
+print('\n Obj file imported successfully ...')
+modifierName='DecimateMod'
+
+print('\n Creating and object list and adding meshes to it ...')
+objectList=bpy.data.objects
+meshes = []
+for obj in objectList:
+  if(obj.type == "MESH"):
+    meshes.append(obj)
+
+print("{} meshes".format(len(meshes)))
+
+for i, obj in enumerate(meshes):
+  bpy.context.view_layer.objects.active = obj
+  print("{}/{} meshes, name: {}".format(i, len(meshes), obj.name))
+  print("{} has {} verts, {} edges, {} polys".format(obj.name, len(obj.data.vertices), len(obj.data.edges), len(obj.data.polygons)))
+  modifier = obj.modifiers.new(modifierName,'DECIMATE')
+  modifier.ratio = decimateRatio
+  modifier.use_collapse_triangulate = True
+  bpy.ops.object.modifier_apply(modifier=modifierName)
+  print("{} has {} verts, {} edges, {} polys after decimation".format(obj.name, len(obj.data.vertices), len(obj.data.edges), len(obj.data.polygons)))
+
+bpy.ops.wm.obj_export(filepath=output_model)
+print('\n Process of Decimation Finished ...')
---
+++ src/blender/blender.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('blender')
+export class BlenderController {}
